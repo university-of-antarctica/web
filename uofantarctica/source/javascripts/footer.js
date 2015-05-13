d54e6e7272a8b14731a76784793829e116a23926
Steven Love <lovestevend@gmail.com>
2015-05-13 08:16:33 -0400

    middleman attempts

---
+++ uofantarctica/source/javascripts/footer.js
@@ -0,0 +1,10 @@
+
+var multi_line_footer_string = String.raw`
+  </div> <!-- end container -->  
+  <div class="col-md-2"></div> 
+  </body> 
+  </html>
+
+`;
+
+document.write(multi_line_footer_string);
